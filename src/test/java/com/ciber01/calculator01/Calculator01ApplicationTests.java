436340ef2c5ce563040bc3fa4a5f5b0842639a52
EnriqueMerino00 <vbz44812@educastur.es>
2024-04-25 18:10:07 +0200

    Añadido esqueleto Spring Boot

---
+++ src/test/java/com/ciber01/calculator01/Calculator01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciber01.calculator01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Calculator01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
